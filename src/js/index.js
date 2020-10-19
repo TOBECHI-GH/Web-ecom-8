52f8381cae10ab4bc2d6f1cc07c5ba6cfc178270
deploy.sh <>
2020-10-19 19:46:03 +0100

    publish: Merge branch 'temp' into Frontend
    generated from commit 69d24a6e90315395432ced6b8e48b895c960177b

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import "jquery";
-import main from "./main";
-import upload from "./upload";
-import invoiceScript from "./script";
-
-console.log("New Frontend dev Scaffold");
-
-$("body").animate({ opacity: 0.5 }, 500).animate({ opacity: 1.0 }, 500);
