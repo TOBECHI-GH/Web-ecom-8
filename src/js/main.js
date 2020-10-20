e0aa2c068502281ccc9c7d08362fc73ac46d4bdb
deploy.sh <>
2020-10-20 06:24:43 +0100

    resolve merge conflict

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-      const menuOpen = document.querySelector('.menu__open');
-      const menuClose = document.querySelector('.menu__close');
-      const mobileNav = document.querySelector('.nav__items')
-
-      menuOpen.addEventListener('click', () => {
-         mobileNav.style.display = 'block';
-         menuOpen.style.display = 'none';
-         menuClose.style.display = 'block';
-      });
-
-      menuClose.addEventListener('click', () => {
-         mobileNav.style.display = 'none';
-         if(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth <= '600') {
-            menuOpen.style.display = 'block';
-         } else {
-            menuOpen.style.display = 'none';
-         }
-         menuClose.style.display = 'none';
-      });
-=======
 const menuOpen = document.querySelector(".menu__open");
 const menuClose = document.querySelector(".menu__close");
 const mobileNav = document.querySelector(".nav__items");
@@ -44,4 +23,3 @@ menuClose &&
     }
     menuClose.style.display = "none";
   });
->>>>>>> e093b8afbdc486a318fd3ea0be9c90bbcacccef8
