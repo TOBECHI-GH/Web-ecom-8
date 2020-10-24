ad0a7e089741dfd887ad3d05d58b1ef227d5218f
Akpan Abraham <56775903+Akpjunior94@users.noreply.github.com>
2020-10-23 19:44:37 -0700

    Update upload.js

--- src/js/upload.js
+++ src/js/upload.js
@@ -1,32 +1,30 @@
-export default (function () {
-  const wrapper = document.querySelector(".wrapper");
-  const fileName = document.querySelector(".file-name");
-  const defaultBtn = document.querySelector("#default-btn");
-  const customBtn = document.querySelector("#custom-btn");
-  const cancelBtn = document.querySelector("#cancel-btn i");
-  const img = document.getElementById("img");
-  let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
-  function defaultBtnActive() {
-    defaultBtn.click();
-  }
+const wrapper = document.querySelector(".wrapper");
+const defaultBtn = document.querySelector("#default-btn");
+const customBtn = document.querySelector("#custom-btn");
+const cancelBtn = document.querySelector("#cancel-btn i");
+const img = document.querySelector("#upload-image");
+let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
 
-  defaultBtn.addEventListener("change", function () {
-    const file = this.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function () {
-        const result = reader.result;
-        img.src = result;
-        wrapper.classList.add("active");
-      };
-      cancelBtn.addEventListener("click", function () {
-        img.src = "";
-        wrapper.classList.remove("active");
-      });
-      reader.readAsDataURL(file);
+function defaultBtnActive(){
+  defaultBtn.click();
+}
+defaultBtn.addEventListener("change", function(){
+  const file = this.files[0];
+  if(file){
+    const reader = new FileReader();
+    reader.onload = function(){
+      const result = reader.result;
+      img.src = result;
+      wrapper.classList.add("active");
     }
-    if (this.value) {
-      let valueStore = this.value.match(regExp);
-    }
-  });
-})();
+    cancelBtn.addEventListener("click", function(){
+      img.src = "";
+      wrapper.classList.remove("active");
+    })
+    reader.readAsDataURL(file);
+  }
+  if(this.value){
+    let valueStore = this.value.match(regExp);
+    fileName.textContent = valueStore;
+  }
+});
